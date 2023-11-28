0df798cb9cc63d2efa0b55d8f7f0101181dbfbb8
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-11-28 12:43:53 -0300

    criando exercicio de entrada e saida de dados

---
+++ exercicios/exercicio_input.py
@@ -0,0 +1,12 @@
+"""
+1 Imprimir na tela a mensagem : 'Olá Mundo!.
+
+#print('Olá, mundo!')
+
+2)Receber nome e idade do usuario e iprimir em um unico print
+"""
+
+nome  = input('Digite seu nome: ')
+idade = input('Digite sua idade:')
+
+print(f'Seu nome é: {nome} voce tem {idade} anos de idade!')
