d0bf8f7c632236270a5bc6c6b1a28d48cd5ec72b
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-11-28 13:03:39 -0300

    conhecenco help e Dir

---
+++ Dir_e_Help.py
@@ -0,0 +1,23 @@
+"""
+Dir e Help
+
+Dir: Apresenta uma lista de funções e metodos que podem ser utilizados
+ em algum  tipo de dado.
+
+sintaxe: dir(valor/tipo de dado)
+
+# print(dir('urso'))
+# print(dir(8))
+# print(dir(float))
+
+#=========================================================
+
+Help: Instrucoes de utilizar funões e metodos disponiveis para determinado
+tipo de dado.
+
+sintaxe: help(funcao) ou help(valor/tipo de dado.metodo/funcao
+"""
+
+
+# print(help(print))
+print(help('dudu'.title()))
